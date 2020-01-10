40cfca7b11d30c587d002c5c5730923afe4e6d1c
Jon Jones <jonjones@Jons-MacBook-Pro.local>
2020-01-09 22:19:51 -0600

    Formatting and introduction of profile concerns to the header.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,24 +1,33 @@
 import { h } from "preact";
 import { Link } from "preact-router/match";
-import style from './style.scss';
+import style from "./style.scss";
 
 const Header: preact.FunctionalComponent = () => {
-    return (
-        <header class={style.header}>
-            <nav>
-                <Link activeClassName={style.active} href="/">
-                    Home
-                </Link>
-            </nav>
-            <h1>Recipe App Name</h1>
-            <nav>
-
-                <Link activeClassName={style.active} href="/profile">
-                    Me
-                </Link>
-            </nav>
-        </header>
-    );
+  return (
+    <header class={style.header}>
+      <nav>
+        <Link activeClassName={style.active} href="/">
+          Home
+        </Link>
+      </nav>
+      <h1>Recipe App Name</h1>
+      <nav>
+        <Link
+          activeClassName={style.none}
+          className={style.none}
+          href="/profile"
+        >
+          Profile
+        </Link>
+        <Link activeClassName={style.active} href="/profile">
+          Login
+        </Link>
+        <Link activeClassName={style.active} href="/profile">
+          Sign Up
+        </Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
